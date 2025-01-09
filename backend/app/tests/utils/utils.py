40b0a1dbae3cdb8f59970596ae5a0be6858b8fb9
KKhajiit <junesup0402@gmail.com>
2025-01-09 16:51:24 +0900

    feat: 테스트용 서버 데이터를 랜덤하게 생성하는 함수 추가

--- backend/app/tests/utils/utils.py
+++ backend/app/tests/utils/utils.py
@@ -5,6 +5,13 @@ from fastapi.testclient import TestClient
 
 from app.core.config import settings
 
+# random names for servers
+word_list = [
+    "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", 
+    "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", 
+    "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor", "Whiskey", 
+    "X-ray", "Yankee", "Zulu"
+]
 
 def random_lower_string() -> str:
     return "".join(random.choices(string.ascii_lowercase, k=32))
@@ -12,6 +19,18 @@ def random_lower_string() -> str:
 
 def random_email() -> str:
     return f"{random_lower_string()}@{random_lower_string()}.com"
+
+
+def random_ip_address():
+    return '.'.join(str(random.randint(0, 255)) for _ in range(4)) 
+
+
+def random_server_name():
+    return random.choice(word_list) + "-" + random.choice(word_list)
+
+
+def random_port():
+    return random.randint(1024, 65535)
 
 
 def get_superuser_token_headers(client: TestClient) -> dict[str, str]:

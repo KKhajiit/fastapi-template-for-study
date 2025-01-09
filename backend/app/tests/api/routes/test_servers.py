2d6ddfe5a4c53fd6ffe28ddb955e82158e75c4fc
KKhajiit <junesup0402@gmail.com>
2025-01-09 16:51:53 +0900

    feat: server CRUD를 테스트하는 코드

--- backend/app/tests/api/routes/test_servers.py
+++ backend/app/tests/api/routes/test_servers.py
@@ -1,18 +1,22 @@
 import uuid
+
 from fastapi.testclient import TestClient
-from sqlmodel import Session
+from sqlmodel import Session, select
+
+from app import crud
 from app.core.config import settings
 from app.models import Server, ServerCreate
-from app.main import app
-from app.api.deps import get_db
-from app.tests.utils.utils import get_superuser_token_headers
-from app.tests.utils.server import create_random_server
+from app.tests.utils.utils import random_ip_address, random_server_name, random_port
 
 
 def test_create_server(
-        client: TestClient, superuser_token_headers: dict[str, str]
+        client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
-    data = {"ip_address": "123.456.789.0"}
+    ip_address = random_ip_address
+    name = random_server_name
+    port = random_port
+
+    data = {"ip_address": ip_address, "name": random_server_name, "port": random_port}
     r = client.post(
         f"{settings.API_V1_STR}/servers/",
         headers=superuser_token_headers,
@@ -20,75 +24,84 @@ def test_create_server(
     )
     assert 200 <= r.status_code < 300
     created_server = r.json()
-    assert created_server["ip_address"] == "123.456.789.0"
-
-# Setup FastAPI test client
-client = TestClient(app)
-
-# Dependency override for test database session
-# def override_get_db():
-#     from app.tests.utils.session import get_test_session
-#     yield from get_test_session()
-
-app.dependency_overrides[get_db] = get_db
+    server = crud.get_server_by_ip(session=db, ip_address=ip_address)
+    assert server
+    assert server.ip_address == created_server["ip_address"]
 
 
-# def test_create_server(session: Session):
-#     superuser_headers = get_superuser_token_headers(client, session)
-#     server_data = {
-#         "ip_address": "192.168.1.1",
-#     }
+def test_get_existing_server(
+        client: TestClient, superuser_token_headers: dict[str, str], db: Session
+) -> None:
+    ip_address = random_ip_address
+    name = random_server_name
+    port = random_port
+    server_in = ServerCreate(ip_address=ip_address, name=name, port=port)
+    server = crud.create_server(session=db, server_create=server_in)
+    server_id = server.id
 
-#     response = client.post("/servers/", json=server_data, headers=superuser_headers)
-#     assert response.status_code == 200
-#     created_server = response.json()
-#     assert created_server["ip_address"] == server_data["ip_address"]
-
-
-def test_read_servers(session: Session):
-    superuser_headers = get_superuser_token_headers(client, session)
-    create_random_server(session)
-
-    response = client.get("/servers/", headers=superuser_headers)
-    assert response.status_code == 200
-    servers = response.json()
-    assert "data" in servers
-    assert "count" in servers
-    assert len(servers["data"]) > 0
-
-
-def test_read_server_by_id(session: Session):
-    superuser_headers = get_superuser_token_headers(client, session)
-    server = create_random_server(session)
-
-    response = client.get(f"/servers/{server.id}", headers=superuser_headers)
-    assert response.status_code == 200
-    fetched_server = response.json()
-    assert fetched_server["id"] == str(server.id)
-
-
-def test_update_server(session: Session, client: TestClient):
-    superuser_headers = get_superuser_token_headers(client, session)
-    server = create_random_server(session)
-    update_data = {"ip_address": "192.168.1.2"}
-
-    response = client.patch(
-        f"/servers/{server.id}", json=update_data, headers=superuser_headers
+    r = client.get(
+        f"{settings.API_V1_STR}/servers/{server_id}",
+        headers=superuser_token_headers,
     )
-    assert response.status_code == 200
-    updated_server = response.json()
-    assert updated_server["ip_address"] == update_data["ip_address"]
+    assert 200 <= r.status_code < 300
+    api_server = r.json()
+
+    existing_server = crud.get_server_by_ip(session=db, ip_address=ip_address)
+    assert existing_server
+    assert existing_server.ip_address == api_server["ip_address"]
+
+    existing_server = crud.get_server_by_name(session=db, name=name)
+    assert existing_server
+    assert existing_server.name == api_server["name"]
 
 
-def test_delete_server(session: Session):
-    superuser_headers = get_superuser_token_headers(client, session)
-    server = create_random_server(session)
+def test_retrieve_users(
+    client: TestClient, superuser_token_headers: dict[str, str], db: Session
+) -> None:
+    ip_address1 = random_ip_address
+    name1 = random_server_name
+    port1 = random_port
+    server_in1 = ServerCreate(ip_address=ip_address1, name=name1, port=port1)
+    crud.create_server(session=db, server_create=server_in1)
 
-    response = client.delete(f"/servers/{server.id}", headers=superuser_headers)
-    assert response.status_code == 200
-    message = response.json()
-    assert message["message"] == "Server deleted successfully"
+    ip_address2 = random_ip_address
+    name2 = random_server_name
+    port2 = random_port
+    server_in2 = ServerCreate(ip_address=ip_address2, name=name2, port=port2)
+    crud.create_server(session=db, server_create=server_in2)
 
-    # Ensure server is deleted
-    response = client.get(f"/servers/{server.id}", headers=superuser_headers)
-    assert response.status_code == 404
+    r = client.get(f"{settings.API_V1_STR}/servers/", headers=superuser_token_headers)
+    all_servers = r.json()
+
+    assert len(all_servers["data"]) > 1
+    assert "count" in all_servers
+    for item in all_servers["data"]:
+        assert "ip_address" in item
+        assert "name" in item
+        assert "port" in item
+
+def test_update_user(
+    client: TestClient, superuser_token_headers: dict[str, str], db: Session
+) -> None:
+    ip_address = random_ip_address
+    name = random_server_name
+    port = random_port
+    server_in = ServerCreate(ip_address=ip_address, name=name, port=port)
+    server = crud.create_server(session=db, server_create=server_in)
+
+    data = {"name": "Updated_server_name"}
+    r = client.patch(
+        f"{settings.API_V1_STR}/servers/{server.id}",
+        headers=superuser_token_headers,
+        json=data,
+    )
+    assert r.status_code == 200
+    updated_server = r.json()
+
+    assert updated_server["name"] == "Updated_server_name"
+
+    server_query = select(Server).where(Server.ip_address == ip_address)
+    server_db = db.exec(server_query).first()
+    db.refresh(server_db)
+    assert server_db
+    assert server_db.name == "Updated_server_name"
